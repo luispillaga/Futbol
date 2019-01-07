aa091698443acd800d745b3c1584344fe3a94d9c
Luis Pillaga <lpillaga@gmail.com>
2019-01-07 12:11:23 -0500

    Primer commit

---
+++ Futbol/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Futbol.Startup))]
+namespace Futbol
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
