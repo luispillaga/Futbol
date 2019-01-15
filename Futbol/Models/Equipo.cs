7024b2f272d3f606a150cbdd42339da0f4899028
Daniel <canino9242@hotmail.com>
2019-01-14 22:01:13 -0500

    ya agrege modelo equipo y jugador

---
+++ Futbol/Models/Equipo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Futbol.Models
+{
+    public class Equipo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Representante { get; set; }
+        public string Celular { get; set; }
+        public string Telefono { get; set; }
+        public Imagen Escudo { get; set; }
+        public int ImagenId { get; set; }
+        public string Estado { get; set; }
+
+        [NotMapped]
+        public HttpPostedFileBase ImageFile { get; set; }
+    }
+}
---
+++ Futbol/Models/Jugador.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Futbol.Models
+{
+    public class Jugador
+    {
+        public int Id { get; set; }
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public string Estado { get; set; }
+        public Imagen Foto { get; set; }
+        public int ImagenId { get; set; }
+        public string Dorsal { get; set; }
+        public Equipo Equipo { get; set; }
+        public int EquipoId { get; set; }
+        [NotMapped]
+        public HttpPostedFileBase ImageFile { get; set; }
+    }
+}
