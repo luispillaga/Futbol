d529c741a53bfec9d5e800c37f683ba947264f31
Daniel <canino9242@hotmail.com>
2019-01-16 09:08:47 -0500

    Agregadas las opciones de la barra izquierda y el controldaor y vistas de equipos

---
+++ Futbol/Controllers/EquiposController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Futbol;
+
+namespace Futbol.Controllers
+{
+    public class EquiposController : Controller
+    {
+        private FutbolEntities db = new FutbolEntities();
+
+        // GET: Equipos
+        public ActionResult Index(int? id_torneo)
+        {
+            var equipo = db.Equipo.Include(e => e.Imagen);
+            IEnumerable<Equipo> equipos_torneo;
+
+
+
+            
+            return View(equipo.ToList());
+        }
+
+        // GET: Equipos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipo equipo = db.Equipo.Find(id);
+            if (equipo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipo);
+        }
+
+        // GET: Equipos/Create
+        public ActionResult Create()
+        {
+            ViewBag.imagen_id = new SelectList(db.Imagen, "imagen_id", "imagen_title");
+            return View();
+        }
+
+        // POST: Equipos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "equipo_id,equipo_nombre,equipo_representante,equipo_celular,equipo_telefono,equipo_estado,imagen_id")] Equipo equipo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Equipo.Add(equipo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.imagen_id = new SelectList(db.Imagen, "imagen_id", "imagen_title", equipo.imagen_id);
+            return View(equipo);
+        }
+
+        // GET: Equipos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipo equipo = db.Equipo.Find(id);
+            if (equipo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.imagen_id = new SelectList(db.Imagen, "imagen_id", "imagen_title", equipo.imagen_id);
+            return View(equipo);
+        }
+
+        // POST: Equipos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "equipo_id,equipo_nombre,equipo_representante,equipo_celular,equipo_telefono,equipo_estado,imagen_id")] Equipo equipo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(equipo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.imagen_id = new SelectList(db.Imagen, "imagen_id", "imagen_title", equipo.imagen_id);
+            return View(equipo);
+        }
+
+        // GET: Equipos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipo equipo = db.Equipo.Find(id);
+            if (equipo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipo);
+        }
+
+        // POST: Equipos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Equipo equipo = db.Equipo.Find(id);
+            db.Equipo.Remove(equipo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
